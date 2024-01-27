3678e5b8776cbc37f690263f2cd293e9c92d1052
scoxty <xty1821901@163.com>
2024-01-27 12:33:04 +0800

    解决合并冲突

--- backendcloud/backend/src/test/java/com/xty/backend/MyTest.java
+++ backendcloud/backend/src/test/java/com/xty/backend/MyTest.java
@@ -11,14 +11,16 @@ public class MyTest {
     public void testPassword() {
         //创建解析器
         PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = "AI10086";
+        System.out.println(encoder.encode(password));
 
         //对密码进行加密
 //        String password = encoder.encode("123456");
 //        System.out.println("------------"+password);
 
-        String s = "$2a$10$25Ty/Z2LvFaV/TK9/Q1DR.KW7FMEe5Zyj4f9mFrJWIAl6iO/DYTvy";
-
-        System.out.println(encoder.matches("", s));
+//        String s = "$2a$10$25Ty/Z2LvFaV/TK9/Q1DR.KW7FMEe5Zyj4f9mFrJWIAl6iO/DYTvy";
+//
+//        System.out.println(encoder.matches("", s));
 //        //判断原字符加密后和内容是否匹配
 //        boolean result = encoder.matches("123",password);
 //        System.out.println("============="+result);
